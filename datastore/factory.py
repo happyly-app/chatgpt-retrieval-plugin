a29ed2079d34346768f5c9ab53b115f85fdec334
Manuel Zapata <manuel@meltstudio.co>
2023-05-23 16:59:44 -0500

    fix: remove pattern matching from py3.10

--- datastore/factory.py
+++ datastore/factory.py
@@ -6,54 +6,53 @@ async def get_datastore() -> DataStore:
     datastore = os.environ.get("DATASTORE")
     assert datastore is not None
 
-    match datastore:
-        case "chroma":
-            from datastore.providers.chroma_datastore import ChromaDataStore
+    if datastore == "chroma":
+        from datastore.providers.chroma_datastore import ChromaDataStore
 
-            return ChromaDataStore()
-        case "llama":
-            from datastore.providers.llama_datastore import LlamaDataStore
+        return ChromaDataStore()
+    if datastore == "llama":
+        from datastore.providers.llama_datastore import LlamaDataStore
 
-            return LlamaDataStore()
+        return LlamaDataStore()
 
-        case "pinecone":
-            from datastore.providers.pinecone_datastore import PineconeDataStore
+    if datastore == "pinecone":
+        from datastore.providers.pinecone_datastore import PineconeDataStore
 
-            return PineconeDataStore()
-        case "weaviate":
-            from datastore.providers.weaviate_datastore import WeaviateDataStore
+        return PineconeDataStore()
+    if datastore == "weaviate":
+        from datastore.providers.weaviate_datastore import WeaviateDataStore
 
-            return WeaviateDataStore()
-        case "milvus":
-            from datastore.providers.milvus_datastore import MilvusDataStore
+        return WeaviateDataStore()
+    if datastore == "milvus":
+        from datastore.providers.milvus_datastore import MilvusDataStore
 
-            return MilvusDataStore()
-        case "zilliz":
-            from datastore.providers.zilliz_datastore import ZillizDataStore
+        return MilvusDataStore()
+    if datastore == "zilliz":
+        from datastore.providers.zilliz_datastore import ZillizDataStore
 
-            return ZillizDataStore()
-        case "redis":
-            from datastore.providers.redis_datastore import RedisDataStore
+        return ZillizDataStore()
+    if datastore == "redis":
+        from datastore.providers.redis_datastore import RedisDataStore
 
-            return await RedisDataStore.init()
-        case "qdrant":
-            from datastore.providers.qdrant_datastore import QdrantDataStore
+        return await RedisDataStore.init()
+    if datastore == "qdrant":
+        from datastore.providers.qdrant_datastore import QdrantDataStore
 
-            return QdrantDataStore()
-        case "azuresearch":
-            from datastore.providers.azuresearch_datastore import AzureSearchDataStore
+        return QdrantDataStore()
+    if datastore == "azuresearch":
+        from datastore.providers.azuresearch_datastore import AzureSearchDataStore
 
-            return AzureSearchDataStore()
-        case "supabase":
-            from datastore.providers.supabase_datastore import SupabaseDataStore
+        return AzureSearchDataStore()
+    if datastore == "supabase":
+        from datastore.providers.supabase_datastore import SupabaseDataStore
 
-            return SupabaseDataStore()
-        case "postgres":
-            from datastore.providers.postgres_datastore import PostgresDataStore
+        return SupabaseDataStore()
+    if datastore == "postgres":
+        from datastore.providers.postgres_datastore import PostgresDataStore
 
-            return PostgresDataStore()
-        case _:
-            raise ValueError(
-                f"Unsupported vector database: {datastore}. "
-                f"Try one of the following: llama, pinecone, weaviate, milvus, zilliz, redis, or qdrant"
-            )
+        return PostgresDataStore()
+
+    raise ValueError(
+        f"Unsupported vector database: {datastore}. "
+        f"Try one of the following: llama, pinecone, weaviate, milvus, zilliz, redis, or qdrant"
+    )
